23c2d3d4e6dd84b51874f1a3b60cad1bfabfe7d8
Sriram8788 <54744038+Sriram8788@users.noreply.github.com>
2024-08-22 12:46:38 +0530

    changed app.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,7 +2,10 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Maven!");
+        System.out.println("Hello World!");
+    }
+
+    public int add(int a, int b) {
+        return a + b;
     }
 }
-

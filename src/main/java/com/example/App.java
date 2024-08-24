27d2b357328ae7af4dc0d8b7c967e63500ab0030
Sriram8788 <54744038+Sriram8788@users.noreply.github.com>
2024-08-24 20:07:48 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,11 +1,25 @@
-package com.example;
-
+package com.devopshint.StringPalindrome;
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
 
-    public int add(int a, int b) {
-        return a + b;
-    }
+	public boolean isPalindrome(String input) {
+
+		if (input == null) {
+			throw new IllegalArgumentException("input shouldn't be null");
+		}
+
+		if (input.equals(reverse(input))) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	private String reverse(String input) {
+		String rev = "";
+		for (int i = input.length() - 1; i >= 0; i--) {
+			rev = rev + input.charAt(i);
+		}
+		return rev;
+	}
+
 }
